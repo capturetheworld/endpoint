48305f0389eaf37e3530cec98f7a45b4066118cc
Ian SooHoo <ian@iansoohoo.me>
2022-11-18 09:13:20 -0800

    initial

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Page from './components/search'
+import './App.css';
+
+function App() {
+  return (
+    <Page/>
+    
+  );
+}
+
+export default App;
---
+++ src/components/search.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react';
+
+
+const Page = () => {
+    const [greeting, setGreeting] = useState(
+        'Hello Function Component!'
+    );
+
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
+
+    // Note: the empty deps array [] means
+    // this useEffect will run once
+    // similar to componentDidMount()
+    useEffect(() => {
+        fetch("https://api.example.com/items")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    setItems(result);
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+    }, [])
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Loading...</div>;
+    } else {
+        return (
+            <ul>
+                {items.map(item => (
+                    <li key={item.id}>
+                        {item.name} {item.price}
+                    </li>
+                ))}
+            </ul>
+        );
+    }
+
+
+
+}
+
+
+
+const Headline = ({ headline, onChangeHeadline }) => (
+    <div>
+        <h1>{headline}</h1>
+
+        <input type="text" value={headline} onChange={onChangeHeadline} />
+    </div>
+);
+
+export default Page;
